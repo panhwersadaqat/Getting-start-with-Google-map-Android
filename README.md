d49e5f191bc3c9da8c64a84279f3f822a9615997
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2020-07-12 18:56:15 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Working With MAPS using JAVA
+
+simple demo app for Beginners
+
+## Screenshots
+
+![Screenshot 1](screenshots/screen_1.png)
---
+++ app/src/main/java/com/thefuturestic/demomap/MapsActivity.java
@@ -0,0 +1,38 @@
+package com.thefuturestic.demomap;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng mpk = new LatLng(25.511109, 69.013426);
+        mMap.addMarker(new MarkerOptions().position(mpk).title("Mirpurkhas"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mpk));
+    }
+}
